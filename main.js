739ac5e722ec2797f9aa662da813d7505eb5b1a5
ruochenj001 <noreply8211@gmail.com>
2022-04-09 21:31:51 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,75 @@
+let cWidth = window.innerWidth;
+let cHeight = window.innerHeight;
+let rContainer = document.getElementById("result-container");
+let errorMsg = document.getElementById("err");
+let resultContainer = document.getElementById("result-container");
+let resultElement = document.getElementsByClassName("result")[0];
+
+// correct display
+if (cWidth > 768) {
+	rContainer.style.width = "768px";
+	rContainer.style.marginLeft = "auto";
+	rContainer.style.marginRight = "auto";
+}
+
+errorMsg.innerHTML = "";
+
+// load api key
+gapi.load("client", () => {
+	gapi.client.setApiKey("AIzaSyBqQGSeJZUdI0itB4t-UW21-DOv3Ae1cAk");
+	gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
+		.then(() => console.log("GAPI client loaded for API"), (err) => {
+			// an error occurred
+			errorMsg.innerHTML = "Error: Failed to load GAPI client for API. \nError code: " + err;
+		});
+});
+
+document.getElementById("search-button").onclick = (e) => {
+	// clear results
+	resultContainer.innerHTML = "";
+	resultContainer.appendChild(errorMsg);
+
+	let input = document.getElementById("text-input").value;
+	let maxResults = document.getElementById("option-max-results").value;
+	let order = document.getElementById("option-order").value;
+	let params = {
+		"part": "snippet",
+		"type": "video",
+		"order": order,
+		"maxResults": maxResults,
+		"q": input
+	};
+	gapi.client.youtube.search.list(params).then((result) => {
+		let items = result.result.items;
+		console.log(items);
+		items.forEach((e, i) => {
+			let id = e.id.videoId;
+			let title = e.snippet.title;
+			let description = e.snippet.description;
+			let publishTime = e.snippet.publishTime;
+			let thumbnail = e.snippet.thumbnails.medium;
+			let node = resultElement.cloneNode(true);
+			node.getElementsByClassName("result-preview")[0].src = thumbnail.url;
+			node.getElementsByClassName("result-title")[0].innerHTML = title;
+			node.getElementsByClassName("result-description")[0].innerHTML = description;
+			node.getElementsByClassName("result-publish-time")[0].innerHTML = publishTime;
+			node.style.display = "block";
+			node.onclick = (e) => {
+				let videoContainer = node.getElementsByClassName("video-container")[0];
+				if (videoContainer.style.display == "none") {
+					let video = document.createElement("embed");
+					video.type = "text/plain";
+					video.width = 400;
+					video.height = 300;
+					video.src = "https://youtube.com/embed/" + id;
+					videoContainer.appendChild(video);
+					videoContainer.style.display = "block";
+				} else {
+					videoContainer.innerHTML = "";
+					videoContainer.style.display = "none";
+				}
+			};
+			resultContainer.appendChild(node);
+		});
+	});
+};
