9c6be180f7e153c1f54c86fe7bdcd787757c9be2
ruochenj001 <noreply8211@gmail.com>
2022-04-10 17:37:55 +0100

    Update main.js

--- main.js
+++ main.js
@@ -20,18 +20,39 @@ gapi.load("client", () => {
 	gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
 		.then(() => console.log("GAPI client loaded for API"), (err) => {
 			// an error occurred
-			errorMsg.innerHTML = "Error: Failed to load GAPI client for API. \nError code: " + err;
+			console.log(err);
+			errorMsg.innerHTML = "Error: Failed to load GAPI client for API. Use the developer console to get error details.";
 		});
 });
 
-document.getElementById("search-button").onclick = (e) => {
+document.getElementById("search-button").onclick = (e) => run();
+document.getElementById("text-input").onkeydown = (e) => {
+	if (e.keyCode == 13) // enter
+		run();
+};
+
+function correctNumberRange(element, def, min, max) {
+	let val = element.value;
+	if (val == null || val == "")
+		return element.value = def.toString();
+
+	let nVal = parseInt(val);
+	if (nVal < min)
+		return element.value = min.toString();
+	else if (nVal > max)
+		return element.value = max.toString();
+	else return element.value;
+}
+
+function run() {
 	// clear results
 	resultContainer.innerHTML = "";
 	resultContainer.appendChild(errorMsg);
 
 	let input = document.getElementById("text-input").value;
-	let maxResults = document.getElementById("option-max-results").value;
+	let maxResults = correctNumberRange(document.getElementById("option-max-results"), 10, 1, 50);
 	let order = document.getElementById("option-order").value;
+
 	let params = {
 		"part": "snippet",
 		"type": "video",
@@ -39,37 +60,46 @@ document.getElementById("search-button").onclick = (e) => {
 		"maxResults": maxResults,
 		"q": input
 	};
-	gapi.client.youtube.search.list(params).then((result) => {
-		let items = result.result.items;
-		console.log(items);
-		items.forEach((e, i) => {
-			let id = e.id.videoId;
-			let title = e.snippet.title;
-			let description = e.snippet.description;
-			let publishTime = e.snippet.publishTime;
-			let thumbnail = e.snippet.thumbnails.medium;
-			let node = resultElement.cloneNode(true);
-			node.getElementsByClassName("result-preview")[0].src = thumbnail.url;
-			node.getElementsByClassName("result-title")[0].innerHTML = title;
-			node.getElementsByClassName("result-description")[0].innerHTML = description;
-			node.getElementsByClassName("result-publish-time")[0].innerHTML = publishTime;
-			node.style.display = "block";
-			node.onclick = (e) => {
-				let videoContainer = node.getElementsByClassName("video-container")[0];
-				if (videoContainer.style.display == "none") {
-					let video = document.createElement("embed");
-					video.type = "text/plain";
-					video.width = 400;
-					video.height = 300;
-					video.src = "https://youtube.com/embed/" + id;
-					videoContainer.appendChild(video);
-					videoContainer.style.display = "block";
-				} else {
-					videoContainer.innerHTML = "";
-					videoContainer.style.display = "none";
-				}
-			};
-			resultContainer.appendChild(node);
+
+	try {
+		gapi.client.youtube.search.list(params).then((result) => {
+			try {
+				result.result.items.forEach((e, i) => {
+					let id = e.id.videoId;
+					let title = e.snippet.title;
+					let description = e.snippet.description;
+					let publishTime = e.snippet.publishTime;
+					let thumbnail = e.snippet.thumbnails.medium;
+					let node = resultElement.cloneNode(true);
+					node.getElementsByClassName("result-preview")[0].src = thumbnail.url;
+					node.getElementsByClassName("result-title")[0].innerHTML = title;
+					node.getElementsByClassName("result-description")[0].innerHTML = description;
+					node.getElementsByClassName("result-publish-time")[0].innerHTML = publishTime;
+					node.style.display = "block";
+					node.onclick = (e) => {
+						let videoContainer = node.getElementsByClassName("video-container")[0];
+						if (videoContainer.style.display == "none") {
+							let video = document.createElement("embed");
+							video.type = "text/plain";
+							video.width = 400;
+							video.height = 300;
+							video.src = "https://youtube.com/embed/" + id;
+							videoContainer.appendChild(video);
+							videoContainer.style.display = "block";
+						} else {
+							videoContainer.innerHTML = "";
+							videoContainer.style.display = "none";
+						}
+					};
+					resultContainer.appendChild(node);
+				});
+			} catch(err) {
+				console.log(err);
+				errorMsg.innerHTML = "Error: Failed to fetch search results. Use the developer console to get error details.";
+			}
 		});
-	});
-};
+	} catch(err) {
+		console.log(err);
+		errorMsg.innerHTML = "Error: Failed to fetch search results. Use the developer console to get error details.";
+	}
+}
