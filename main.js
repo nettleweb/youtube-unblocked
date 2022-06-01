f20081fa6a93710b6c4f5f3724ec5d83bfd7b837
ruochenj001 <jrcdonghai@gmail.com>
2022-06-01 23:55:53 +0100

    feature update + ui improvement

--- main.js
+++ main.js
@@ -1,23 +1,31 @@
 "use strict";
 
 (() => {
+// default error handler
+window.onerror = (msg, src, lineno, colno, err) => {
+	alert(msg, "Error", "images/error.png");
+};
+
+
 // load api key
 gapi.load("client", () => {
 	gapi.client.setApiKey("AIzaSyBqQGSeJZUdI0itB4t-UW21-DOv3Ae1cAk");
 	gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
 		.then(() => console.log("GAPI client loaded for API"), (err) => {
 			console.log(err);
-			alert("Failed to load GAPI client for API. Use the developer console to get error details.", "Error", "error.png");
+			alert("Failed to load GAPI client for API. Use the developer console to get error details.", "Error", "images/error.png");
 		});
 });
 
 let cWidth = document.body.clientWidth;
+let pageToken = null;
 let resultContainer = document.getElementById("result-container");
 let resultElement = document.getElementsByClassName("result")[0];
 let textInput = document.getElementById("text-input");
 let searchButton = document.getElementById("search-button");
 let optionMaxResults = document.getElementById("option-max-results");
 let optionOrder = document.getElementById("option-order");
+let loadmoreButton = document.getElementById("loadmore-button");
 
 // correct display
 if (cWidth < 800) {
@@ -27,15 +35,22 @@ if (cWidth < 800) {
 		if (cWidth < 400) {
 			let logo = document.getElementById("logo");
 			logo.style.width = "100%";
-			logo.style.height = logo.clientWidth / 4 * 3;
+			logo.style.height = Math.round(logo.clientWidth / 4 * 3) + "px";
 		}
 	}
 }
 
-searchButton.onclick = () => run();
+searchButton.onclick = () => {
+	resultContainer.innerHTML = "";
+	pageToken = null;
+	run();
+};
 textInput.onkeydown = (e) => {
-	if (e.keyCode == 13) // enter
+	if (e.keyCode == 13) { // enter
+		resultContainer.innerHTML = "";
+		pageToken = null;
 		run();
+	}
 };
 
 function correctNumberRange(element, def, min, max) {
@@ -52,35 +67,51 @@ function correctNumberRange(element, def, min, max) {
 }
 
 function run() {
-	resultContainer.innerHTML = "";
 	let input = textInput.value;
 	let maxResults = correctNumberRange(optionMaxResults, 10, 1, 50);
 	let order = optionOrder.value;
 	search(input, maxResults, order);
 }
 
-function createVideoFrame(id) {
-	let frame = document.createElement("iframe");
-	frame.style.position = "absolute";
-	frame.style.display = "block";
-	frame.style.width = "100%";
-	frame.style.height = "100%";
-	frame.style.border = "none";
-	frame.setAttribute("allowfullscreen", "true");
-	frame.setAttribute("loading", "lazy");
-	frame.setAttribute("scrolling", "no");
-	frame.onload = () => {
-		let url = new URL("https://www.youtube.com/embed/" + id);
-		let win = frame.contentWindow;
-		if (win.location.href != url.href)
-			win.location = url;
-		Object.freeze(win.location);
-		frame.onload = null;
-	};
-	return frame;
+function vid(parent, id) {
+	// create video frame
+	let it = document.createElement("div");
+	it.id = "video-" + id;
+	it.style.position = "absolute";
+	it.style.display = "block";
+	it.style.width = "100%";
+	it.style.height = "100%";
+	it.style.border = "none";
+	parent.appendChild(it);
+	playVideo(it.id, id);
 }
 
-function search(query, limit, order) {
+function playVideo(fid, id) {
+	let player = new YT.Player(fid, {
+		videoId: id,
+		width: 800,
+		height: 600,
+		playerVars: {
+			autoplay: 1,
+			playsinline: 1,
+			controls: 1,
+			showinfo: 1
+		},
+		events: {
+			onReady: () => onPlayerReady(player),
+			onStateChange: () => onPlayerStateChange(player)
+		}
+	});
+}
+
+function onPlayerReady(player) {
+	player.setVolume(50);
+}
+
+function onPlayerStateChange(player) {
+}
+
+function search(query, limit, order, pageId) {
 	let params = {
 		"part": "snippet",
 		"type": "video",
@@ -88,6 +119,9 @@ function search(query, limit, order) {
 		"maxResults": limit,
 		"q": query
 	};
+
+	if (pageToken != null)
+		params.pageToken = pageToken;
 
 	try {
 		gapi.client.youtube.search.list(params).then((result) => {
@@ -98,7 +132,8 @@ function search(query, limit, order) {
 				console.log(err);
 				alert("Failed to fetch search results. Use the developer console to get error details.", "Error");
 			}
-			r.forEach((e, i) => {
+			for (let i = 0; i < r.length; i++) {
+				let e = r[i];
 				let id = e.id.videoId;
 				let title = e.snippet.title;
 				let description = e.snippet.description;
@@ -109,23 +144,41 @@ function search(query, limit, order) {
 				node.getElementsByClassName("result-title")[0].innerHTML = title;
 				node.getElementsByClassName("result-description")[0].innerHTML = description;
 				node.getElementsByClassName("result-publish-time")[0].innerHTML = publishTime;
-				node.getElementsByClassName("result-item")[0].onclick = (e) => {
-					let videoContainer = node.getElementsByClassName("video-container")[0];
+
+				let videoContainer = node.getElementsByClassName("video-container")[0];
+				// correct display for video container
+				if (resultContainer.clientWidth < 400)
+					videoContainer.style.width = "100%";
+
+				node.getElementsByClassName("result-item")[0].onclick = () => {
 					if (videoContainer.style.display == "none") {
-						videoContainer.appendChild(createVideoFrame(id));
 						videoContainer.style.display = "block";
+						vid(videoContainer, id);
 					} else {
 						videoContainer.innerHTML = "";
 						videoContainer.style.display = "none";
 					}
 				};
+
 				node.style.display = "block";
 				resultContainer.appendChild(node);
-			});
+			}
+
+			loadmoreButton.remove();
+			loadmoreButton.style.display = "block"; // always reset display style to fix some issues
+
+			let nextPageToken = result.result.nextPageToken;
+			if (nextPageToken != null) { // only show when there are more videos available
+				loadmoreButton.onclick = () => {
+					pageToken = nextPageToken;
+					run();
+				};
+				resultContainer.appendChild(loadmoreButton);
+			}
 		});
 	} catch(err) {
 		console.log(err);
-		alert("Failed to fetch search results. Use the developer console to get error details.", "Error", "error.png");
+		alert("Failed to fetch search results. Use the developer console to get error details.", "Error", "images/error.png");
 	}
 }
 
