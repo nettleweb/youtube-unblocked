58f6efbf41aef0e1891f157519586e3d46ce7dd0
Ruochen Jia <rjia@rjia.gq>
2023-01-01 23:17:29 +0000

    ui improvement

--- main.js
+++ main.js
@@ -1,56 +1,54 @@
 "use strict";
 
-(() => {
+(async () => {
 // default error handler
-window.onerror = (msg, src, lineno, colno, err) => {
+window.onerror = (message, src, lineno, colno, error) => {
+	alert(`Error at "${src}", line ${lineno}:${colno}: \n${error}`, "Error");
 };
 
 // load api key
 gapi.load("client", () => {
-	gapi.client.setApiKey("AIzaSyBqQGSeJZUdI0itB4t-UW21-DOv3Ae1cAk");
-	gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
-		.then(() => console.log("GAPI client loaded for API"), (err) => {
-			console.warn("Failed to load GAPI", err);
-		});
+	gapi.client.init({
+		apiKey: "AIzaSyBqQGSeJZUdI0itB4t-UW21-DOv3Ae1cAk",
+		discoveryDocs: [
+			"https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest"
+		]
+	});
 });
 
-let cWidth = document.body.clientWidth;
 let pageToken = null;
-let resultContainer = document.getElementById("result-container");
-let resultElement = document.getElementsByClassName("result")[0];
-let textInput = document.getElementById("text-input");
-let searchButton = document.getElementById("search-button");
-let optionMaxResults = document.getElementById("option-max-results");
-let optionOrder = document.getElementById("option-order");
-let loadmoreButton = document.getElementById("loadmore-button");
 
-// correct display
-if (cWidth < 800) {
-	resultContainer.style.width = "100%";
-	if (cWidth < 500) {
-		document.getElementById("container").style.width = "100%";
-	}
-}
+const resultContainer = document.getElementById("results");
+const searchBar = document.getElementById("search");
+const searchButton = document.getElementById("search-button");
+const maxResults = document.getElementById("max-results");
+const order = document.getElementById("order");
+const loadmore = document.getElementById("loadmore");
 
 searchButton.onclick = () => {
 	resultContainer.innerHTML = "";
 	pageToken = null;
 	run();
 };
-textInput.onkeydown = (e) => {
-	if (e.keyCode == 13) { // enter
-		resultContainer.innerHTML = "";
-		pageToken = null;
-		run();
+searchBar.onkeydown = (e) => {
+	if (e.key == "Enter") {
+		e.preventDefault();
+		searchButton.click();
 	}
 };
 
+/**
+ * @param {HTMLInputElement} element 
+ * @param {number} def 
+ * @param {number} min 
+ * @param {number} max 
+ */
 function correctNumberRange(element, def, min, max) {
-	let val = element.value;
-	if (val == null || val == "")
+	const val = element.value;
+	if (val == null || val.length == 0)
 		return element.value = def.toString();
 
-	let nVal = parseInt(val);
+	const nVal = parseInt(val);
 	if (nVal < min)
 		return element.value = min.toString();
 	else if (nVal > max)
@@ -59,99 +57,94 @@ function correctNumberRange(element, def, min, max) {
 }
 
 function run() {
-	let input = textInput.value;
-	let maxResults = correctNumberRange(optionMaxResults, 10, 1, 50);
-	let order = optionOrder.value;
-	search(input, maxResults, order);
+	search(searchBar.value, correctNumberRange(maxResults, 10, 1, 50), order.value);
 }
 
-function vid(parent, id) {
-	// create video frame
-	let it = document.createElement("iframe");
-	it.style.position = "absolute";
-	it.style.display = "block";
-	it.style.width = "100%";
-	it.style.height = "100%";
-	it.style.border = "none";
-	it.setAttribute("type", "text/plain");
-	it.setAttribute("allowfullscreen", "true");
-	it.setAttribute("sandbox", "allow-scripts allow-same-origin");
-	it.setAttribute("loading", "lazy");
-	it.onload = () => {
-		let url = new URL("https://www.youtube.com/embed/" + id);
-		Object.freeze(url);
-		it.contentWindow.location = url;
-		it.onload = null;
-	};
-	parent.appendChild(it);
+/**
+ * @param {HTMLElement} container 
+ * @param {string} videoId 
+ */
+function createVideoFrame(container, videoId) {
+	const frame = document.createElement("iframe");
+	frame.setAttribute("type", "text/plain");
+	frame.setAttribute("width", "800");
+	frame.setAttribute("height", "600");
+	frame.setAttribute("loading", "lazy");
+	frame.setAttribute("scrolling", "no");
+	frame.setAttribute("fetchpriority", "high");
+	frame.setAttribute("allowfullscreen", "true");
+	frame.setAttribute("referrerpolicy", "no-referrer");
+	frame.setAttribute("sandbox", "allow-scripts allow-same-origin");
+
+	const url = new URL("https://www.youtube.com/embed/" + videoId);
+	url.searchParams.set("autoplay", "1");
+	url.searchParams.set("controls", "1");
+	url.searchParams.set("rel", "0");
+	url.searchParams.set("color", "white");
+	frame.setAttribute("src", url.href);
+
+	container.appendChild(frame);
 }
 
-function search(query, limit, order, pageId) {
-	let params = {
-		"part": "snippet",
-		"type": "video",
-		"order": order,
-		"maxResults": limit,
-		"q": query
+/**
+ * @param {string} q 
+ * @param {string | number} maxResults 
+ * @param {string} order 
+ */
+async function search(q, maxResults, order) {
+	const params = {
+		part: "snippet",
+		type: "video",
+		order,
+		maxResults,
+		pageToken,
+		q
 	};
 
-	if (pageToken != null)
-		params.pageToken = pageToken;
+	const result = await gapi.client.youtube.search.list(params);
+	const items = result.result.items;
+	for (const item of items) {
+		const id = item.id.videoId;
+		const title = item.snippet.title;
+		const description = item.snippet.description;
+		const publishTime = item.snippet.publishTime;
+		const thumbnail = item.snippet.thumbnails.medium;
 
-	try {
-		gapi.client.youtube.search.list(params).then((result) => {
-			let r = [];
-			try {
-				r = result.result.items;
-			} catch(err) {
-				console.warn("Failed to fetch search results", err);
+		const container = document.createElement("div");
+		container.className = "result";
+		container.innerHTML = '<div class="result-item"><img class="result-preview" width="160" height="90" /><div class="result-details"><div class="result-title">Example</div><div class="result-description">An example video</div><div class="result-publish-time">A long time ago</div></div></div><div class="video-container"></div>';
+		container.getElementsByClassName("result-preview")[0].setAttribute("src", thumbnail.url);
+		container.getElementsByClassName("result-title")[0].textContent = title;
+		container.getElementsByClassName("result-description")[0].textContent = description;
+		container.getElementsByClassName("result-publish-time")[0].textContent = publishTime;
+
+		const videoContainer = container.getElementsByClassName("video-container")[0];
+		videoContainer.style.display = "none";
+
+		container.getElementsByClassName("result-item")[0].onclick = () => {
+			if (videoContainer.style.display == "none") {
+				videoContainer.style.display = "block";
+				createVideoFrame(videoContainer, id);
+			} else {
+				videoContainer.innerHTML = "";
+				videoContainer.style.display = "none";
 			}
-			for (let i = 0; i < r.length; i++) {
-				let e = r[i];
-				let id = e.id.videoId;
-				let title = e.snippet.title;
-				let description = e.snippet.description;
-				let publishTime = e.snippet.publishTime;
-				let thumbnail = e.snippet.thumbnails.medium;
-				let node = resultElement.cloneNode(true);
-				node.getElementsByClassName("result-preview")[0].src = thumbnail.url;
-				node.getElementsByClassName("result-title")[0].innerHTML = title;
-				node.getElementsByClassName("result-description")[0].innerHTML = description;
-				node.getElementsByClassName("result-publish-time")[0].innerHTML = publishTime;
+		};
 
-				let videoContainer = node.getElementsByClassName("video-container")[0];
-				// correct display for video container
-				if (resultContainer.clientWidth < 400)
-					videoContainer.style.width = "100%";
+		resultContainer.appendChild(container);
+	}
 
-				node.getElementsByClassName("result-item")[0].onclick = () => {
-					if (videoContainer.style.display == "none") {
-						videoContainer.style.display = "block";
-						vid(videoContainer, id);
-					} else {
-						videoContainer.innerHTML = "";
-						videoContainer.style.display = "none";
-					}
-				};
+	loadmore.remove();
+	loadmore.style.display = "block";
 
-				node.style.display = "block";
-				resultContainer.appendChild(node);
-			}
-
-			loadmoreButton.remove();
-			loadmoreButton.style.display = "block"; // always reset display style to fix some issues
-
-			let nextPageToken = result.result.nextPageToken;
-			if (nextPageToken != null) { // only show when there are more videos available
-				loadmoreButton.onclick = () => {
-					pageToken = nextPageToken;
-					run();
-				};
-				resultContainer.appendChild(loadmoreButton);
-			}
-		});
-	} catch(err) {
-		console.warn("Failed to fetch search results", err);
+	const nextPageToken = result.result.nextPageToken;
+	if (nextPageToken != null) {
+		// only show when there are more videos available
+		loadmore.onclick = () => {
+			pageToken = nextPageToken;
+			run();
+		};
+		resultContainer.appendChild(loadmore);
 	}
 }
 
